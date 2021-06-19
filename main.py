d68098607de71cc9309c88b02775b265bc251799
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2021-06-19 15:05:44 +0000

    it works

---
+++ main.py
@@ -0,0 +1,26 @@
+from random import randint
+import requests
+import time
+import os
+
+url = 'https://discord.com/api/v9/channels/855820738653323267/messages'
+
+data = {
+    'content': 'pls beg',
+    'tts': False
+}
+
+headers = {
+    'authorization': os.getenv('authorization')
+}
+
+def apiCall():
+    x = requests.post(url, data = data, headers=headers)
+    print(x)
+
+while True:
+    apiCall()
+    number = randint(45, 100)
+    print(number)
+    time.sleep(number)
+
